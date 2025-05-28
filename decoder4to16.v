e838d9f1f8507191778110e23678c98f3bed4ec3
rudraThakar <141826284+rudraThakar@users.noreply.github.com>
2025-05-28 11:08:27 +0530

    added decoder

---
+++ decoder4to16.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.05.2025 16:01:38
+// Design Name: 
+// Module Name: decoder4to16
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder4to16(
+    input [0:3] inp,
+    output [0:15] out
+    );
+    assign out = 1 << inp;
+endmodule
