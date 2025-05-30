af34aa63d83c17f87dc566373d80a336eebdc2ed
rudraThakar <141826284+rudraThakar@users.noreply.github.com>
2025-05-30 22:49:38 +0530

    Update ro.v

--- ro.v
+++ ro.v
@@ -1,55 +1,24 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.05.2025 14:47:42
-// Design Name: 
-// Module Name: ro
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module ro(input en, output out);
-  wire inp1;
-  wire w1, w2, w3, w4, w5, w6;
-  
-  assign inp1 = 1'b1;
-  
-  and #1 ro_g1(w1, en, inp1);
-  not #1 ro_g2(w2, w1);
-  not #1 ro_g3(w3, w2);
-  not #1 ro_g4(w4, w3);
-  not #1 ro_g5(w5, w4);
-  not #1 ro_g6(inp1, w5);
-//  not ro_g7(inp1, w6);  
 
-  assign out = inp1;
-endmodule
+module ring_oscillator(
+    input wire en,
+    output wire out
+    );
 
-//module ro (
-//    output wire osc_out
-//);
-//    wire [4:0] n;
+    localparam num_inv = 7;
+    genvar i;
 
-//    // Direct gate-level NOT instantiations (no separate module)
-//    not #1 (n[0], n[4]);
-//    not #1 (n[1], n[0]);
-//    not #1 (n[2], n[1]);
-//    not #1 (n[3], n[2]);
-//    not #1 (n[4], n[3]);
+    wire inter_wire[num_inv:0];
 
-//    assign osc_out = n[0]; // Oscillator output
+    generate
+        for (i = 0; i < num_inv; i = i+1) begin
+            inverter inv(.A(inter_wire[i]), .Y(inter_wire[i+1]));
+        end
+    endgenerate
 
-//endmodule
+    and (inter_wire[0], out, en);
+    assign out = inter_wire[num_inv];
 
+endmodule 
