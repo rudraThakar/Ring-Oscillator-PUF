60962bea542100fa51ff0dea7d563e250213f6db
rudraThakar <141826284+rudraThakar@users.noreply.github.com>
2025-06-06 23:35:33 +0530

    Update mux16to1.v
    made the mux systhesiable

--- mux16to1.v
+++ mux16to1.v
@@ -21,10 +21,32 @@
 
 
 module mux16to1 (
-    input [0:15] in,
-    input [0:3] sel,
-    output out
+    input wire[15:0] in,
+    input wire[3:0] sel,
+    output wire out
 );
-    assign out = in[sel];
-    
+    reg temp_out;
+    assign out = temp_out;
+
+    always @ (*) begin
+        case (sel)
+            'd0: temp_out = in[0];
+            'd1: temp_out = in[1];
+            'd2: temp_out = in[2];
+            'd3: temp_out = in[3];
+            'd4: temp_out = in[4];
+            'd5: temp_out = in[5];
+            'd6: temp_out = in[6];
+            'd7: temp_out = in[7];
+            'd8: temp_out = in[8];
+            'd9: temp_out = in[9];
+            'd10: temp_out =in[10];
+            'd11: temp_out =in[11];
+            'd12: temp_out =in[12];
+            'd13: temp_out =in[13];
+            'd14: temp_out = in[14];
+            'd15: temp_out = in[15];
+            default: temp_out = 'bx;
+        endcase
+    end
 endmodule
